3915aeb21f2ba71d95c051bbb6a9e4a33fb91299
Liam <31185529+liamtw22@users.noreply.github.com>
2024-11-02 14:53:07 -0400

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,18 +1,35 @@
 import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/GoogleCard.js',
   output: {
-    file: 'dist/google-card.js',
-    format: 'es'
+    dir: 'dist',
+    format: 'es',
+    filename: 'google-card.js'
   },
   plugins: [
     resolve(),
+    commonjs(),
     babel({
-      exclude: 'node_modules/**'
+      exclude: 'node_modules/**',
+      presets: [
+        [
+          '@babel/preset-env',
+          {
+            targets: {
+              esmodules: true,
+            },
+          },
+        ],
+      ],
     }),
     terser()
+  ],
+  external: [
+    'lit-element',
+    'lit-html'
   ]
 };
