68db2cc0ab7b4b1646cc3528983be61e35d30391
Liam <31185529+liamtw22@users.noreply.github.com>
2024-11-03 21:14:19 -0500

    Update nightMode.js

--- src/styles/nightMode.js
+++ src/styles/nightMode.js
@@ -1,5 +1,5 @@
 // src/styles/nightMode.js
-import { css } from 'lit-element';
+import { css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 export const nightModeStyles = css`
   :host {
