93f8af837beb4257921ae574503ad826b37a6479
Liam <31185529+liamtw22@users.noreply.github.com>
2024-11-03 21:13:56 -0500

    Update background.js

--- src/styles/background.js
+++ src/styles/background.js
@@ -1,5 +1,5 @@
 // src/styles/background.js
-import { css } from 'lit-element';
+import { css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 export const backgroundStyles = css`
   :host {
