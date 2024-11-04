19b2758b03b34aab1160214bf1a5db1893d31806
Liam <31185529+liamtw22@users.noreply.github.com>
2024-11-03 21:14:10 -0500

    Update controls.js

--- src/styles/controls.js
+++ src/styles/controls.js
@@ -1,5 +1,5 @@
 // src/styles/controls.js
-import { css } from 'lit-element';
+import { css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 export const controlsStyles = css`
   :host {
