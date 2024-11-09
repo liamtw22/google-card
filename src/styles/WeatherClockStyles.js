5a54cc292fdfa519909abba1fb47a8a77c9d053f
Liam <31185529+liamtw22@users.noreply.github.com>
2024-11-09 10:28:35 -0500

    Update WeatherClockStyles.js

--- src/styles/WeatherClockStyles.js
+++ src/styles/WeatherClockStyles.js
@@ -1,4 +1,5 @@
-import { css } from "lit-element";
+// src/styles/WeatherClockStyles.js
+import { css } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 export const weatherClockStyles = css`
   .weather-component {
