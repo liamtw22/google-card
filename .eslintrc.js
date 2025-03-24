2a649314eb4948495d80f1b7a8af4b992b88c844
Liam <31185529+liamtw22@users.noreply.github.com>
2025-03-24 14:24:48 -0400

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,28 @@
 module.exports = {
   env: {
     browser: true,
-    es2021: true,
+    es2021: true
   },
-  extends: ['eslint:recommended', 'plugin:prettier/recommended'],
+  extends: [
+    'eslint:recommended'
+  ],
   parserOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
+    ecmaVersion: 'latest',
+    sourceType: 'module'
   },
   rules: {
-    'no-console': 'off',
-    'no-case-declarations': 'off', // Turn off this rule to avoid issues with lexical declarations in switch statements
-    'prettier/prettier': [
-      'error',
-      {
-        endOfLine: 'auto',
-        singleQuote: true,
-        trailingComma: 'es5',
-        printWidth: 100,
-      },
-    ],
+    'no-unused-vars': ['error', { 
+      'argsIgnorePattern': '^_',
+      'varsIgnorePattern': '^_'
+    }],
+    'no-console': ['warn', { 
+      allow: ['warn', 'error'] 
+    }],
+    'quotes': ['error', 'single'],
+    'semi': ['error', 'always']
   },
-  overrides: [
-    {
-      files: ['src/editor.js'],
-      rules: {
-        // Allow unused imports in editor.js specifically for IMAGE_SOURCE_TYPES
-        'no-unused-vars': ['error', { 'varsIgnorePattern': 'IMAGE_SOURCE_TYPES' }]
-      }
-    }
-  ]
+  globals: {
+    customElements: 'readonly',
+    HTMLElement: 'readonly'
+  }
 };
