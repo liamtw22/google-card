da92950dda2da09fe89f8320dabb74b0bd76ec13
Liam <31185529+liamtw22@users.noreply.github.com>
2025-03-24 14:39:26 -0400

    Update GoogleCard.js

--- src/GoogleCard.js
+++ src/GoogleCard.js
@@ -1,5 +1,5 @@
 // src/GoogleCard.js
-import { LitElement, html, css } from 'lit-element';
+import { css, LitElement, html } from "https://unpkg.com/lit-element@2.4.0/lit-element.js?module";
 
 import { DEFAULT_CONFIG, MIN_BRIGHTNESS } from './constants';
 import { sharedStyles } from './styles/SharedStyles';
