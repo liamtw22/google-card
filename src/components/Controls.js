985b2e6c9086661e503df2b08f2495f11f5a9b85
Liam <31185529+liamtw22@users.noreply.github.com>
2025-03-24 18:44:49 -0400

    Update Controls.js

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -214,9 +214,12 @@ export class Controls extends LitElement {
           font-size: 50px;
           width: 50px;
           height: 50px;
-          display: block;
-          color: var(--control-text-color);
+          display: block !important;
+          color: var(--control-text-color) !important;
           pointer-events: none;
+          fill: currentColor;
+          visibility: visible !important;
+          opacity: 1 !important;
         }
 
         /* iOS specific adjustments */
@@ -455,18 +458,19 @@ export class Controls extends LitElement {
         <div class="icon-container">
           <div class="icon-row">
             <button class="icon-button" @click="${(e) => this.toggleBrightnessCard(e)}">
-              <iconify-icon icon="material-symbols-light:sunny-outline-rounded"></iconify-icon>
+              <iconify-icon icon="material-symbols-light:sunny-outline-rounded" style="color: var(--control-text-color);"></iconify-icon>
             </button>
             <button class="icon-button">
-              <iconify-icon icon="material-symbols-light:volume-up-outline-rounded"></iconify-icon>
+              <iconify-icon icon="material-symbols-light:volume-up-outline-rounded" style="color: var(--control-text-color);"></iconify-icon>
             </button>
             <button class="icon-button">
               <iconify-icon
                 icon="material-symbols-light:do-not-disturb-on-outline-rounded"
+                style="color: var(--control-text-color);"
               ></iconify-icon>
             </button>
             <button class="icon-button">
-              <iconify-icon icon="material-symbols-light:alarm-add-outline-rounded"></iconify-icon>
+              <iconify-icon icon="material-symbols-light:alarm-add-outline-rounded" style="color: var(--control-text-color);"></iconify-icon>
             </button>
             <button
               class="icon-button"
@@ -477,7 +481,7 @@ export class Controls extends LitElement {
               @mouseup="${this.handleSettingsIconTouchEnd}"
               @mouseleave="${this.handleSettingsIconTouchEnd}"
             >
-              <iconify-icon icon="material-symbols-light:settings-outline-rounded"></iconify-icon>
+              <iconify-icon icon="material-symbols-light:settings-outline-rounded" style="color: var(--control-text-color);"></iconify-icon>
             </button>
           </div>
         </div>
@@ -487,7 +491,7 @@ export class Controls extends LitElement {
 
   render() {
     return html`
-      <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js"></script>
+      <script src="https://code.iconify.design/iconify-icon/1.0.7/iconify-icon.min.js" async="false"></script>
       <div class="controls-container" @touchstart="${e => e.stopPropagation()}">
         ${this.showOverlay ? this.renderOverlay() : ''}
         ${this.showBrightnessCard ? this.renderBrightnessCard() : ''}
