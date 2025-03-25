c9f81614d731f85696c556e5d096523dd506076f
GitHub Action <action@github.com>
2025-03-25 19:29:00 +0000

    chore: update dist/google-card.js

--- dist/google-card.js
+++ dist/google-card.js
@@ -15,7 +15,7 @@ const DEFAULT_CONFIG = {
   show_weather: !0,
   show_aqi: !0,
   weather_entity: "weather.forecast_home",
-  aqi_entity: "sensor.air_quality_index",
+  aqi_entity: "sensor.ridgewood_air_quality_index",
   light_sensor_entity: "sensor.liam_room_display_light_sensor",
   brightness_sensor_entity: "sensor.liam_room_display_brightness_sensor"
 }, sharedStyles = css`
